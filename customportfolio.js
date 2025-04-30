36ea946811115556631309ba9f11c44e6caab559
Sahil-1498 <sahil123tp@gmail.com>
2025-04-30 22:35:26 +0530

    Update customportfolio.js

--- customportfolio.js
+++ customportfolio.js
@@ -14,3 +14,81 @@
             }
         });
     });
+
+
+    document.addEventListener('DOMContentLoaded', function() 
+    {
+        const projectHolder = document.querySelector('.project-holder');
+        const project = document.querySelectorAll('.project');
+        const leftArrowScroll = document.querySelector('.left-arow-scroll');
+        const rightArrowScroll = document.querySelector('.right-arow-scroll');
+
+        const scrollAmount = 1305;
+
+        rightArrowScroll.addEventListener('click', () => {
+            projectHolder.scrollBy({
+                left: scrollAmount,
+                behavior: 'smooth'
+            }) 
+        }) ;
+
+        leftArrowScroll.addEventListener('click', () => {
+            projectHolder.scrollBy({
+                left: -scrollAmount,
+                behavior: 'smooth'
+            }) 
+        }) ;
+
+
+        // leftScroll.style.display = 'none';
+
+        projectHolder.addEventListener('scroll', () => {
+            const atStart = projectHolder.scrollLeft === 0;
+            const atEnd = projectHolder.scrollLeft + projectHolder.clientWidth >= projectHolder.scrollWidth - 1;
+            //  leftScroll.style.display = atStart ? 'none' : 'block';
+            //  rightScroll.style.display = atStart ? 'none' : 'block';
+        }) ;
+
+
+    });
+
+
+
+
+
+
+    document.addEventListener('DOMContentLoaded', function() 
+    {
+        const exlWrks = document.querySelector('.exl-wrks');
+        const exlFrame = document.querySelectorAll('.exl-frame');
+        const leftScroll = document.querySelector('.left-scroll');
+        const rightScroll = document.querySelector('.right-scroll');
+
+        const scrollAmount = 1105;
+
+        rightScroll.addEventListener('click', () => {
+            exlWrks.scrollBy({
+                left: scrollAmount,
+                behavior: 'smooth'
+            }) 
+        }) ;
+
+        leftScroll.addEventListener('click', () => {
+            exlWrks.scrollBy({
+                left: -scrollAmount,
+                behavior: 'smooth'
+            }) 
+        }) ;
+
+
+        // leftScroll.style.display = 'none';
+
+        exlWrks.addEventListener('scroll', () => {
+            const atStart = exlWrks.scrollLeft === 0;
+            const atEnd = exlWrks.scrollLeft + exlWrks.clientWidth >= exlWrks.scrollWidth - 1;
+            //  leftScroll.style.display = atStart ? 'none' : 'block';
+            //  rightScroll.style.display = atStart ? 'none' : 'block';
+        }) ;
+
+
+    });
